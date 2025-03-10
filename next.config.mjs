a56dde588b25331b4af83c21e374111ffb2f721c
speedybeaver <gmandwee@gmail.com>
2025-03-10 00:31:38 -0400

    removed lcl.host (unecessary)

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,2 @@
-// next.config.mjs
-
-import autoCert from "anchor-pki/auto-cert/integrations/next";
-
-const withAutoCert = autoCert({
-  enabledEnv: "development",
-});
-
 const nextConfig = {};
-
-export default withAutoCert(nextConfig);
+export default nextConfig;
